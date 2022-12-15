9bff4178391868a7ccf98423e4adb27b7d8c3041
W3Charles <www3charles@gmail.com>
2022-12-15 04:11:25 -0500

    feat: add Testimonial header, contact info and form

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -9,7 +9,40 @@ import './Footer.scss';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <h2 className="head-text"> Take a coffee & chat with me </h2>
+
+      <div className="app__footer-cards">
+        <div className="app__footer-card">
+          <img src={images.email} alt="email" />
+          <a href="mailto:hello@charlessunday.com" className="p-text">hello@charlessunday.com</a>
+        </div>
+        <div className="app__footer-card">
+          <img src={images.mobile} alt="phone" />
+          <a href="tel: +1 (123) 456-789" className="p-text">+1 (123) 456-789m</a>
+        </div>
+      </div>
+
+      <div className="app__footer-form app__flex">
+        <div className="app__flex">
+          <input className="p-text" type="text" placeholder="Your Name" name="name" value={username} onChange={handleChangeInput} />
+        </div>
+        <div className="app__flex">
+          <input className="p-text" type="email" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput} />
+        </div>
+      </div>
+      <div>
+        <textarea
+          className="p-text"
+          placeholder="Your Message"
+          value={message}
+          name={message}
+          onChange={handleChangeInput}
+        />
+      </div>
+      <button type="button" className="p-text" onClick={handleSubmit}>Send Message</button>
+
+    </>
   )
 }
 
